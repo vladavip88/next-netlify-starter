26409fe06e76557d70eac509270db7abd0fc8e01
Vladimir Prsendic <vladavip88@gmail.com>
2021-06-05 22:16:24 +0200

    small update

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import styles from '@styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div className={styles.root}>
+        <h1 className={styles.title}>Pogrebna Oprema Cajić</h1>
+        <p className={styles.text}>Uskoro...</p>
     </div>
   )
 }
